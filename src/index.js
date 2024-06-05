af507b451740b3a1c9f2a3247e77ac8f328a92e5
ryo <ryo@indeed.com>
2024-06-05 16:50:36 +0900

    US-03 - Game flow

--- src/index.js
+++ src/index.js
@@ -97,7 +97,13 @@ function chooseHole(holes) {
  */
 function gameOver() {
   // TODO: Write your code here
-
+  if (time > 0) {
+    timeoutId = showUp()
+    return timeoutId
+  } else {
+    gameStopped = stopGame()
+    return gameStopped
+  }
 }
 
 /**
@@ -110,8 +116,8 @@ function gameOver() {
  *
  */
 function showUp() {
-  let delay = 0; // TODO: Update so that it uses setDelay()
-  const hole = 0; // TODO: Update so that it use chooseHole()
+  let delay = setDelay(difficulty); // TODO: Update so that it uses setDelay()
+  const hole = chooseHole(holes); // TODO: Update so that it use chooseHole()
   return showAndHide(hole, delay);
 }
 
@@ -125,12 +131,12 @@ function showUp() {
  */
 function showAndHide(hole, delay) {
   // TODO: call the toggleVisibility function so that it adds the 'show' class.
-
+  toggleVisibility(hole);
   const timeoutID = setTimeout(() => {
     // TODO: call the toggleVisibility function so that it removes the 'show' class when the timer times out.
-
+    toggleVisibility(hole);
     gameOver();
-  }, 0); // TODO: change the setTimeout delay to the one provided as a parameter
+  }, delay); // TODO: change the setTimeout delay to the one provided as a parameter
   return timeoutID;
 }
 
@@ -142,7 +148,7 @@ function showAndHide(hole, delay) {
  */
 function toggleVisibility(hole) {
   // TODO: add hole.classList.toggle so that it adds or removes the 'show' class.
-
+  hole.classList.toggle("show")
   return hole;
 }
 
@@ -255,8 +261,8 @@ function stopGame() {
  *
  */
 function startGame() {
-  //setDuration(10);
-  //showUp();
+  setDuration(10);
+  showUp();
   return "game started";
 }
 
